28c377564fab100da40c9e015ed0e732ace8df9c
Training <viswakarpurapu@gmail.com>
2020-11-11 13:54:52 +0530

    book

--- app/src/main/java/com/example/app2/AsyncActivity.java
+++ app/src/main/java/com/example/app2/AsyncActivity.java
@@ -5,28 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class AsyncActivity extends AppCompatActivity {
+import com.example.app2.FetchBook;
 
+public class AsyncActivity extends AppCompatActivity {
     private static final String TAG = AsyncActivity.class.getSimpleName();
     ProgressBar progressBar;
-    TextView mBookInput;
+    EditText mBookInput;
+    TextView mTitleText,mAuthorText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async);
         progressBar = findViewById(R.id.progressBar);
-        mBookInput = findViewById(R.id.bookinput);
-        String queryString = mBookInput.getText().toString();
+        mBookInput = findViewById(R.id.bookInput);
+        mTitleText = findViewById(R.id.titleText);
+        mAuthorText = findViewById(R.id.textViewauthor);
     }
 
     public void handleClick(View view) {
         Log.i(TAG,"handleClick");
-        //download something from internet
+      /*  //download something from internet
         DownloadTask downloadTask = new DownloadTask(progressBar);
         downloadTask.execute("https://urlForimagetobedownloaded");
-        //show the download progress on the progressbar
+        //show the download progress on the progressbar*/
+        String queryString = mBookInput.getText().toString();
+
+        new FetchBook(mTitleText, mAuthorText).execute(queryString);
     }
 }
---
+++ app/src/main/java/com/example/app2/FtechBook.java
@@ -0,0 +1,54 @@
+package com.example.app2;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import com.example.app2.NetworkUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+class FetchBook extends AsyncTask<String,Void,String> {
+    private TextView mTitleText;
+    private TextView mAuthorText;
+    public FetchBook(TextView mTitleText, TextView mAuthorText) {
+        this.mTitleText = mTitleText;
+        this.mAuthorText = mAuthorText;
+    }
+    @Override
+    protected String doInBackground(String... params) {
+        return NetworkUtils.getBookInfo(params[0]);
+    }
+    @Override
+    protected void onPostExecute(String s)
+    {
+        super.onPostExecute(s);
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+            //Iterate through the results
+            for(int i = 0; i<itemsArray.length(); i++){
+                JSONObject book = itemsArray.getJSONObject(i); //Get the current item
+                String title=null;
+                String authors=null;
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+//If both a title and author exist, update the TextViews and return
+                if (title != null && authors != null){
+                    mTitleText.setText(title);
+                    mAuthorText.setText(authors);
+                    return;
+                }
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
